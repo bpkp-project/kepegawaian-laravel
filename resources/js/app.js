996923b0a5699d17e72ebaa645856dd1543961cc
AgungDH <agungdh@live.com>
2025-08-01 09:40:29 +0700

    feat(frontend): migrate from Bootstrap/Tailwind to Alpine.js, Axios, SweetAlert2 & Toastr
    - Remove Bootstrap JS import and TailwindCSS plugin
    - Add Toastr CSS import in resources/css/app.css
    - Import and expose Alpine, axios, SweetAlert2 and toastr in resources/js/app.js
    - Set axios default `X-Requested-With` header
    - Update vite.config.js to drop the tailwindcss() plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
-import './bootstrap';
+import Alpine from 'alpinejs';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import toastr from 'toastr';
+
+window.Alpine = Alpine;
+window.axios = axios;
+window.Swal = Swal;
+window.toastr = toastr;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
